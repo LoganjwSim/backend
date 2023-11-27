0b15825db10e2aa2a12dd2b809a90d7a699825bd
LoganjwSim <j1sim234@gmail.com>
2023-11-27 15:12:45 +0900

    Complete

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 const port = 3010;
 
-app.use(express.json());
+app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 app.use("/todos", todosRouter);
 
